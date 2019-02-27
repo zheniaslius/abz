7e491b9aa08af3265bffa77373d1a07bbc6765b2
evsliusar <sluisarchuk@gmail.com>
2019-02-27 17:24:06 +0200

    Updates

---
+++ precache-manifest.dd86650bbf3ae9906adc2adcdd0ef103.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "524cb8b8b9c6b6437860",
+    "url": "/static/js/main.524cb8b8.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cf4e131c2615c42dd3fc",
+    "url": "/static/css/2.7c952b19.chunk.css"
+  },
+  {
+    "revision": "cf4e131c2615c42dd3fc",
+    "url": "/static/js/2.cf4e131c.chunk.js"
+  },
+  {
+    "revision": "fa3dcef844df21f4734cf55187374c6c",
+    "url": "/static/media/logo.fa3dcef8.svg"
+  },
+  {
+    "revision": "a5e64440cd3ce17ca542be43f24bdba7",
+    "url": "/static/media/bg-1.a5e64440.jpg"
+  },
+  {
+    "revision": "2afa77717943906cc812fb6a7a97aeee",
+    "url": "/static/media/man-mobile.2afa7771.svg"
+  },
+  {
+    "revision": "d882d855ad0c93a45176c40e0dacf8ce",
+    "url": "/static/media/html.d882d855.svg"
+  },
+  {
+    "revision": "2963787969ffcb9cc7324947abf540d5",
+    "url": "/static/media/css.29637879.svg"
+  },
+  {
+    "revision": "cf53a810c6682334aa9a2e46a4b86ce4",
+    "url": "/static/media/javascript.cf53a810.svg"
+  },
+  {
+    "revision": "7f1bc2b0c5fe5340f0f1ce80732e2791",
+    "url": "/static/media/bg-2.7f1bc2b0.jpg"
+  },
+  {
+    "revision": "03656e6bce429a8711072054919e5d7d",
+    "url": "/static/media/man-laptop-v1.03656e6b.svg"
+  },
+  {
+    "revision": "120ef9987012fe3556542272f37628ec",
+    "url": "/static/media/caret-down.120ef998.svg"
+  },
+  {
+    "revision": "ae7e47ddc289dd05a4c7a852a0ae6e06",
+    "url": "/static/media/mail.ae7e47dd.svg"
+  },
+  {
+    "revision": "ca2f60a309344b3d4e767bc00e31b45e",
+    "url": "/static/media/phone.ca2f60a3.svg"
+  },
+  {
+    "revision": "1a0fe3e3710b4b959ac49eed96689591",
+    "url": "/static/media/cellphone.1a0fe3e3.svg"
+  },
+  {
+    "revision": "a9a773c48e0c7cece9d6e1cf49abad7e",
+    "url": "/static/media/sign-out.a9a773c4.svg"
+  },
+  {
+    "revision": "3b500131c4e9086a78a4490f6abc3f07",
+    "url": "/static/media/line-menu.3b500131.svg"
+  },
+  {
+    "revision": "e331f32d288074ecad87de7ccab0138b",
+    "url": "/static/media/upload.e331f32d.svg"
+  },
+  {
+    "revision": "20caa7d0c0ff0f6b546395ee0b916408",
+    "url": "/static/media/facebook.20caa7d0.svg"
+  },
+  {
+    "revision": "569eb74ac7a0deb9585e7fc04f9cde0c",
+    "url": "/static/media/linkedin.569eb74a.svg"
+  },
+  {
+    "revision": "f263dbf1758175dcc7fb5e925373cd68",
+    "url": "/static/media/instagram.f263dbf1.svg"
+  },
+  {
+    "revision": "3e993ec30ed135283d2b632b79c009fa",
+    "url": "/static/media/twitter.3e993ec3.svg"
+  },
+  {
+    "revision": "603e7eb001182ac09947fe8e50c3bb96",
+    "url": "/static/media/pinterest.603e7eb0.svg"
+  },
+  {
+    "revision": "eaffb1be002517af3b863c0e1778dcab",
+    "url": "/index.html"
+  }
+];
